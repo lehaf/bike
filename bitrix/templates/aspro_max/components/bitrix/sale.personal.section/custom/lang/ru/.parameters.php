880c1315bace6118b19a1bb51bfaa2acb7e179ee
Ann Kedich <keda20021002@gmail.com>
2024-09-18 10:35:25 +0300

    edit personal page

---
+++ bitrix/templates/aspro_max/components/bitrix/sale.personal.section/custom/lang/ru/.parameters.php
@@ -0,0 +1,6 @@
+<?
+$MESS['SHOW_ADS_PAGE_TITLE'] = 'Вывести ссылку на страницу с объявлениями';
+$MESS['PATH_TO_ADS_TITLE'] = 'Путь к странице объявлений';
+$MESS['SHOW_BONUS_PAGE_TITLE'] = 'Вывести ссылку на страницу бонусов';
+$MESS['PATH_TO_BONUS_TITLE'] = 'Путь к странице бонусов';
+?>
