4a70631381507940fc65a030e6823a9b1287405c
server viking <serverviking@mail.com>
2024-12-24 10:51:15 +0300

    new element commit

--- ajax/call_form_custom.php
+++ ajax/call_form_custom.php
@@ -1,24 +1,15 @@
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php"); ?>
-<<<<<<< HEAD
 <div class="jqmOverlay"
      style="height: 100%; width: 100%; position: fixed; left: 0px; top: 0px; z-index: 2999; opacity: 0.5;"></div>
 <div class="callback-custom_frame jqmWindow popup jqm-init scrollblock show _modal-new"
      style="z-index: 3000; opacity: 1;">
-=======
-<div class="jqmOverlay" style="height: 100%; width: 100%; position: fixed; left: 0px; top: 0px; z-index: 2999; opacity: 0.5;"></div>
-<div class="callback-custom_frame jqmWindow popup jqm-init scrollblock show _modal-new" style="z-index: 3000; opacity: 1;">
->>>>>>> c80798c (update aspro)
     <div>
         <a href="#"
            class="close jqmClose"><?= CMax::showIconSvg('', SITE_TEMPLATE_PATH . '/images/svg/Close.svg') ?></a>
         <div class="_form-modal">
             <div class="_form_head">
                 <div class="form_head-text">
-<<<<<<< HEAD
                     <?= $name ?>
-=======
-                    Имя и фамилия продавца / название компании
->>>>>>> c80798c (update aspro)
                 </div>
                 <div class="form_head__img">
                     <img src="/upload/CMax/logo-form.png" alt="">
@@ -28,7 +19,6 @@
                 Скажите продавцу, что звоните с Bike.by.
             </div>
             <div class="_form-text">
-<<<<<<< HEAD
                 Если после разговора вы выяснили, что объявление неактуальное или недостоверное,
                 <div class="form-popup" data-event="claim-form">сообщите нам</div>
             </div>
@@ -44,23 +34,6 @@
                     </div>
                 <?php endif; ?>
 
-=======
-                Если после разговора вы выяснили, что объявление неактуальное или недостоверное, <div class="form-popup" data-event="claim-form" >сообщите нам</div>
-            </div>
-            <div class="_form-phone">
-                <div class="_form-phone__title">Светлана</div>
-                <div class="_form-phone__list">
-                    <div class="_form-phone__el">
-                        <a href="tel:+37529123-12-12">+ 375 (29) 123-12-12</a>
-                    </div>
-                    <div class="_form-phone__el">
-                        <a href="tel:+37529123-12-12">+ 375 (29) 123-12-12</a>
-                    </div>
-                    <div class="_form-phone__el">
-                        <a href="tel:+37529123-12-12">+ 375 (29) 123-12-12</a>
-                    </div>
-                </div>
->>>>>>> c80798c (update aspro)
             </div>
         </div>
     </div>
@@ -73,7 +46,6 @@
         $(this).closest('.callback-custom_frame').remove();
         $('.jqmOverlay').remove();
     })
-<<<<<<< HEAD
 
     $('.form-popup').on('click', function (e) {
         $.ajax({
@@ -84,39 +56,13 @@
                 $('.callback-custom_frame').html(response);
             },
             error: function (xhr, status, error) {
-=======
-    // $(document).on('click', '.jqmOverlay', function (e) {
-    //     e.preventDefault();
-    //     $(this).closest('#popup_iframe_wrapper').removeAttr('style');
-    //     $('.callback-custom_frame').remove();
-    //     $(this).remove();
-    // })
-
-    $('.form-popup').on('click', function(e){
-        $.ajax({
-            url: '/ajax/form_custom.php',
-            data: {type: $(this).attr('data-event')},
-            type: 'GET',
-            success: function(response){
-                $('.callback-custom_frame').html(response);
-            },
-            error: function(xhr, status, error){
->>>>>>> c80798c (update aspro)
                 console.log(error);
             }
         });
     });
-<<<<<<< HEAD
     $(".jqmOverlay").on("click", function () {
         $("#popup_iframe_wrapper").css("display", "none");
         $(".callback-custom_frame").remove();
     });
 
-=======
-    $(".jqmOverlay").on("click" , function (){
-        $("#popup_iframe_wrapper").css("display", "none");
-        $(".callback-custom_frame").removeClass("show")
-    });
-    
->>>>>>> c80798c (update aspro)
 </script>
