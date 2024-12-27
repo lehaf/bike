35971ea2ee88900a8b906688d1c137b3227a4a56
Ann Kedich <keda20021002@gmail.com>
2024-12-27 12:47:54 +0300

    documentation : new structure info

--- local/modules/web.doc/admin/structure.php
+++ local/modules/web.doc/admin/structure.php
@@ -17,13 +17,34 @@ $APPLICATION->SetAdditionalCSS("/bitrix/css/" . $mid . "/style.css");
 
 </div>
 <h3 style="color: red">Если слетит после обновления аспро, то перезалить следующие файлы:</h3>
-<div style="margin-bottom: 50px">
+<div style="margin-bottom: 30px">
     <p>Файлы для изменения сортировки в каталоге (по дате создания):</p>
     <p>bitrix/templates/aspro_max/components/bitrix/catalog/main/lang/ru/.parameters.php - название пункта в настройках компонента</p>
     <p>bitrix/templates/aspro_max/components/bitrix/catalog/main/lang/ru/section.php - название пункта на странице каталога</p>
     <p>bitrix/templates/aspro_max/components/bitrix/catalog/main/.parameters.php - добавлен пункт для сортировки по DATE_CREATE в параметр SORT_BUTTONS</p>
     <p>bitrix/templates/aspro_max/components/bitrix/catalog/main/sort.php - добавлена логика для корректного отображения нового пункта сортировки "Дата создания" (DATE_CREATE)</p>
 </div>
+
+<div style="margin-bottom: 50px">
+    В файле <i>bitrix/templates/aspro_max/components/bitrix/catalog/main/elements.php</i> убрать подобный вызов метода process404:
+    <pre>
+        $arElement = CMaxCache::CIBLockElement_GetList(array('CACHE' => array("MULTI" =>"N", "TAG" => CMaxCache::GetIBlockCacheTag($arParams["IBLOCK_ID"]))), CMax::makeElementFilterInRegion($arElementFilter), false, false, array("ID", "IBLOCK_ID", "IBLOCK_SECTION_ID", "NAME", "PREVIEW_TEXT", "PREVIEW_PICTURE", "DETAIL_PICTURE", "PROPERTY_ASSOCIATED_FILTER", "PROPERTY_EXPANDABLES_FILTER", "PROPERTY_ASSOCIATED", "PROPERTY_EXPANDABLES"));
+
+        if(!$arElement)
+        {
+            \Bitrix\Iblock\Component\Tools::process404(
+                ""
+                ,($arParams["SET_STATUS_404"] === "Y")
+                ,($arParams["SET_STATUS_404"] === "Y")
+                ,($arParams["SHOW_404"] === "Y")
+                ,$arParams["FILE_404"]
+            );
+        }
+    </pre>
+    для работы функционала захода на неактивные обявления пользователем, который их создал
+</div>
+
+
 
 <h3>Кроны</h3>
 <div>
