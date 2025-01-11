bc5c63b683d864aba2e526d5023649c173e9c834
Ann Kedich <keda20021002@gmail.com>
2025-01-11 12:53:40 +0300

    feature : add account number to user at registration

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,9 +1,11 @@
 <?php
 if (file_exists(__DIR__.'/vendor/autoload.php')) require_once __DIR__.'/vendor/autoload.php';
 \Bitrix\Main\Loader::registerAutoLoadClasses(null, [
-    'classes\Filter' => '/local/php_interface/classes/Filter.php'
+    'classes\Filter' => '/local/php_interface/classes/Filter.php',
+    'classes\Page' => '/local/php_interface/classes/Page.php'
 ]);
 
 if (file_exists(__DIR__.'/include/constants.php')) require_once __DIR__.'/include/constants.php';
 if (file_exists(__DIR__.'/include/functions.php')) require_once __DIR__.'/include/functions.php';
+if (file_exists(__DIR__.'/include/constants.php')) require_once __DIR__.'/include/events.php';
 
