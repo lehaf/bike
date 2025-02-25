070d551b9418d0842a529cfb7f88fe9cebbb58dd
Ann Kedich <keda20021002@gmail.com>
2025-02-25 16:54:06 +0300

    refactor : edit text on brand page

--- bitrix/templates/aspro_max/components/bitrix/news/partners/lang/ru/detail.php
+++ bitrix/templates/aspro_max/components/bitrix/news/partners/lang/ru/detail.php
@@ -5,7 +5,7 @@ $MESS['BACK_LINK'] = 'Список брендов';
 $MESS['T_REVIEWS'] = 'Отзывы';
 $MESS['T_DOCS'] = 'Рекомендательные письма';
 $MESS['T_PROJECTS'] = 'Проекты';
-$MESS['T_GOODS'] = 'Товары #BRAND_NAME# в нашем магазине';
+$MESS['T_GOODS'] = 'Объявления #BRAND_NAME#';
 $MESS['T_GOODS_SECTION'] = 'Разделы с товарами #BRAND_NAME# в нашем магазине';
 $MESS['CATALOG_SMART_FILTER_TITLE'] = 'Фильтр';
 
